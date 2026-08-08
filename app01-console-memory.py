3b2f5c0648bbc530df4cf1bbc97d7eda5fbac529
Chanut Piyapongkowit <165237770+Newcpto@users.noreply.github.com>
2026-08-08 16:37:14 +0700

    Update app01-console-memory.py

--- app01-console-memory.py
+++ app01-console-memory.py
@@ -350,21 +350,6 @@ class RiskAssessmentTestSuite:
 
 
 # =====================================================================
-# SYSTEM APPLICATION ENTRY POINT
-# =====================================================================
-if __name__ == "__main__":
-    if RUN_TEST_SUITE:
-        suite = RiskAssessmentTestSuite(model_factory=PatientModel, service_class=ClinicalRiskService)
-        suite.run_all_tiers()
-    
-    # Run in console with in-memory model
-    db_model = PatientModel()
-    rules_service = ClinicalRiskService()
-    ui_view = ConsoleView()
-    
-    app = ConsoleController(model=db_model, service=rules_service, view=ui_view)
-    app.run()
-# =====================================================================
 # SYSTEM APPLICATION ENTRY POINT (STREAMLIT)
 # =====================================================================
 if __name__ == "__main__":
