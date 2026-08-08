8f5e197f414fe21b3455af4575cbed08cb212245
Chanut Piyapongkowit <165237770+Newcpto@users.noreply.github.com>
2026-08-08 17:02:19 +0700

    Update jj.py

--- jj.py
+++ jj.py
@@ -10,7 +10,7 @@ import streamlit as st
 RUN_TEST_SUITE = True
 
 # =====================================================================
-# 1. DATA ACCESS LAYER (MODELS - SQLITE3 PERSISTENCE)
+# 1. DATA ACCESS LAYER (MODELS - SQLITE3 DATABASE)
 # =====================================================================
 class PatientModel:
     """Manages persistent SQLite database patient data storage and initial data cleaning."""
